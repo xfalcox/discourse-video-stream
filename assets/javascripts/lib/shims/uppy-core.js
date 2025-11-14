45a5b97f8a03af906789679fd6eae3cb9e958e8a
Rafael Silva <xfalcox@gmail.com>
2025-11-14 14:52:45 -0300

    Fix shim imports for vendored Uppy

--- assets/javascripts/lib/shims/uppy-core.js
+++ assets/javascripts/lib/shims/uppy-core.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 /* global define, requirejs */
 
-import BasePlugin from "../vendor/@uppy/core/BasePlugin.js";
-import EventManager from "../vendor/@uppy/core/EventManager.js";
+import BasePlugin from "../vendor/@uppy/core/BasePlugin";
+import EventManager from "../vendor/@uppy/core/EventManager";
 
 function registerUppyModule(moduleName, moduleValue) {
   if (requirejs?.entries?.[moduleName]) {
